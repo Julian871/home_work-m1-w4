f810648709d36f985f4d1eaeb2ace0ea97693cd3
Julian871 <vindiesel.1999@mail.ru>
2023-09-19 18:51:13 +0300

    correct auth

--- src/middlewares/authorization.ts
+++ src/middlewares/authorization.ts
@@ -22,7 +22,6 @@ export const authMiddleware = async (req: Request, res: Response, next: NextFunc
     if(userId) {
         req.user = await usersService.getUserById(userId)
         next()
-        return
     }
     res.send(401)
 }
