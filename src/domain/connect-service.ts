73bc4b30277ea029b94ccdfc798916e10a8efda8
Julian871 <vindiesel.1999@mail.ru>
2023-11-02 18:00:03 +0300

    correct-counter 1.0

--- src/domain/connect-service.ts
+++ src/domain/connect-service.ts
@@ -4,19 +4,14 @@ import {v4 as uuidv4} from "uuid";
 
 export const connectService = {
 
-    async countConnection(IP: string, URL: string) {
-        return await connectRepositories.countConnection(IP, URL)
-    },
-
     async createConnection(IP: string, URL: string, deviceName: string) {
         const connectInformation = {
             IP: IP,
             URL: URL,
             lastActiveDate: +new Date,
             deviceName: deviceName,
-            deviceId: uuidv4()
+            deviceId: uuidv4(),
         }
-
         await connectRepositories.createConnectionInfo(connectInformation)
     }
 }
--- src/middlewares/connect.ts
+++ src/middlewares/connect.ts
@@ -1,5 +1,6 @@
 import {NextFunction, Request, Response} from "express";
 import {connectService} from "../domain/connect-service";
+import {connectRepositories} from "../repositories/connect-repositories";
 
 
 export const checkConnect = async (req: Request, res: Response, next: NextFunction) => {
@@ -7,12 +8,13 @@ export const checkConnect = async (req: Request, res: Response, next: NextFuncti
     const URL = req.originalUrl
     const deviceName = req.headers['user-agent'] || 'hacker'
 
-    const countConnect = await connectService.countConnection(IP, URL )
+    const countConnect = await connectRepositories.countConnection(IP, URL)
     console.log('count: ', countConnect)
     if(countConnect >= 5) {
         await connectService.createConnection(IP, URL, deviceName)
         return res.sendStatus(429)
     } else {
+        await connectService.createConnection(IP, URL, deviceName)
         return next()
     }
 }
