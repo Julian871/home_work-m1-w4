aa8609ec6351a86ad553d6c68db81058b8a2b8ed
Julian871 <vindiesel.1999@mail.ru>
2023-11-07 14:10:16 +0300

    correct request to db

--- src/domain/users-service.ts
+++ src/domain/users-service.ts
@@ -137,7 +137,7 @@ export const usersService = {
             let passwordSalt = await bcrypt.genSalt(10)
             let passwordHash = await authService._generateHash(password, passwordSalt)
             await usersRepositories.recoveryPassword(passwordSalt, passwordHash, recoveryCode)
-            await usersRepositories.invalidRecoveryCode(recoveryCode)
+            await usersRepositories.invalidRecoveryCode(findRecoveryCode._id)
             return true
         }
     },
